7f7f56803453ee45b6054fdf3471afe830a18c96
mateosalta <mateo_salta@yahoo.com>
2014-07-10 14:03:41 -0500

    updated to use hcitool

--- BLARG.sh
+++ BLARG.sh
@@ -1,33 +1,43 @@
-#!/bin/sh
-##################################
-#                                #
-# BLARG bluecasting shell script #
-#                                #
-# Usage: BLARG [filename]        #
-##################################
-bt-device -l > device
-while true; do
-#touch device
-#bt-device -l > device
-#loop until device is paired
-#until [ "$(cat device | sed -e '1!d')" = "Added devices:" ];
-while [ "$(cat device | sed -e '1!d')" = "No devices found" ];
-do
-	# command for finding device
-        sleep 10
-	bt-device -l > device
-        cat device | sed -e '2!d' | sed 's/.*(//' | sed -e 's/)//' > address
-	
-done
-sleep 5
-	#if [ $(cat device | sed -e '1!d') = "No devices found"; then
-	#	continue 
+#!/bin/bash
+###################################
+#                                 #
+# BLARG bluecasting shell script  #
+#                                 #
+# Usage: BLARG [filename]         #
+#                                 # 
+# If no argument given will give  #
+# file selection dialog           #
+#                                 #
+# Disconnect requires sudo        #
+###################################
 
-	# sed magic to make the device output usable, cuts the second line 
-	# then removeal of device sent to to free up next device in list
-	#cat device | sed -e '2!d' | sed 's/.*(//' | sed -e 's/)//' > address
-bluetooth-sendto --device="$(< address)" $1
-bt-device -r "$(< address)"
-bluez-test-device remove "$(< address)"
-bt-device -l > device
+
+while true; do
+	#search for connected devices, edit output into usable addresses
+	hcitool con | sed -e '1d' | sed 's/\(.\{7\}\)//' | sed 's/ .*//' > file
+	
+	#text showing if there are no connected devices	
+	echo "Scaning for Connected Devices..."
+	if [ "$(cat file)" = "" ]
+	then echo "No Devices Found"
+	fi
+	sleep 2
+
+#cat file for line by line loop
+filename='file'
+filelines=`cat $filename`
+
+#loop for each line, send file and disconnect each divice
+for line in $filelines ; do
+	echo "Device Found: $line"
+	echo "Sending file..."
+	#send to device	
+	bluetooth-sendto --device="$line" $1
+	sleep 1	
+	echo "Done"
+	#disconnect device, hcitool dc requires root privilages	
+	hcitool dc $line
+	sleep 1	
+	echo "Device Disconected"
+done	
 done
