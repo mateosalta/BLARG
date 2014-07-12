e75069f6b2b05aaab17f50dbec6346b475fe4507
mateosalta <mateo_salta@yahoo.com>
2014-07-12 00:23:28 -0500

    good enough for now, more testing with two phones

--- BLARG.sh
+++ BLARG.sh
@@ -36,33 +36,45 @@ echo -e "This is free software, and you are welcome"
 echo -e "to redistribute it under certain conditions;"
 sleep 2
 
+function morejobs() {
+    while [ $(jobs -p | wc -l) -ge $1 ]; do
+        sleep 1
+    done
+}
 
 while true; do
+	unset line
 	#search for connected devices, edit output into usable addresses
-	hcitool con | sed -e '1d' | sed 's/\(.\{7\}\)//' | sed 's/ .*//' > file
+	hcitool con | sed -e '1d' | sed 's/\(.\{7\}\)//' | sed 's/ .*//' > file 
 	
 	#text showing if there are no connected devices	
 	echo -e "\033[00mScaning for Connected Devices..."
+	
+	if [ "$(pidof bluetooth-sendto)" ]
+	then cat file | sed -e '1d' > file
+	fi
+
 	if [ "$(cat file)" = "" ]
 	then echo "No Devices Found"
 	fi
-	sleep 2
+	sleep 3
 
 #cat file for line by line loop
 filename='file'
 filelines=`cat $filename`
 
-#loop for each line, send file and disconnect each divice
-for line in $filelines ; do
-	echo -e "\033[32mDevice Found: $line"
-	echo -e "\033[00mSending file..."
-	#send to device	
-	bluetooth-sendto --device="$line" $1
-	sleep 1	
-	echo "Done"
-	#disconnect device, hcitool dc requires root privilages	
-	hcitool dc $line
-	sleep 1	
-	echo "Device Disconected"
-done	
+	#loop for each line, send file and disconnect each divice
+	for line in $filelines ; do
+		echo -e "\033[32mDevice Found: $line" && echo -e "\033[00mSending file..." && bluetooth-sendto --device="$line" $1 &
+	morejobs 2 
+	done
+
+		sleep 2
+		if [ -z "$(echo -n $(pidof bluetooth-sendto))" ]
+		then hcitool dc $line 19
+		fi
+		
+	
+	done
+		
 done
