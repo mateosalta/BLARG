6d5ee9e67fceab8e50f64acf1dbfa49625509bcc
mateosalta <mateo_salta@yahoo.com>
2014-07-13 20:54:25 -0500

    switched to list for detecting open connection

--- BLARG.sh
+++ BLARG.sh
@@ -45,16 +45,24 @@ function morejobs() {
 
 while true; do
 	
+	#restet line variable, start fresh for loop	
+	#unset line
+	
 	#search for connected devices, edit output into usable addresses
 	hcitool con | sed -e '1d' | sed 's/\(.\{7\}\)//' | sed 's/ .*//' > file
 	echo -e "\033[00mScaning for Connected Devices..."
 	
 	#count how many connections open, remove devices that are currently sending
-	v=`echo -n "$(pidof bluetooth-sendto)" | wc -w`
-	if [ "$(pidof bluetooth-sendto)" ]
-	then cat file | sed -e '1,'$v\d > file
-	fi
+	#v=`echo -n "$(pidof bluetooth-sendto)" | wc -w`
+	#if [ "$(pidof bluetooth-sendto)" ]
+	#then cat file | sed -e '1,'$v\d > file
+	#fi
 
+	#if address in "list"(device allready trying connection) remove from "file"
+	if [ "$(pidof bluetooth-sendto)" ]
+	then sed -i "/^${list}/d" file
+	fi
+	
 	#report no new devices found
 	if [ "$(cat file)" = "" ]
 	then echo "No Devices Found"
@@ -67,19 +75,20 @@ while true; do
 
 		#loop for each line, send file and disconnect each divice
 		for line in $filelines ; do
-			echo -e "\033[32mDevice Found: $line" && echo -e "\033[00mSending file..." && bluetooth-sendto --device="$line" $1 && wait && hcitool dc $line 19 && echo -e "\033[31mDevice Disconnected: $line" &
-		morejobs 2 
+			echo "$line" >> list
+			echo -e "\033[32mDevice Found: $line" && echo -e "\033[00mSending file..." && bluetooth-sendto --device="$line" $1 && echo "$line" >> list && wait && hcitool dc $line 19 && echo -e "\033[31mDevice Disconnected: $line" && sed -i '/'$line'/d' ./list &
+		
+		#put in simultanus jobs, 7 limit of adapter - may need to be adjusted		
+		morejobs 7
  		done
 		
 	#if window is open for longer than 55 seconds (after send time out, or user rejected) kill
 	if [ "$(pidof bluetooth-sendto)" ]
 	then a=`pidof bluetooth-sendto | cut -d' ' -f1`
-	     b=`ps -p $a -o etime | sed -e '2!d' | sed -e 's/^[ \t]*//' | cut -d':' -f2`
-	     if [[ $b -gt 55 ]]
-	     then kill -SIGKILL $a
-	     fi
+	b=`ps -p $a -o etime | sed -e '2!d' | sed -e 's/^[ \t]*//' | cut -d':' -f2`
+		if [[ $b -gt 55 ]]
+		then kill -SIGKILL $a
+		fi
 	fi
-
-	done
-		
+	
 done
